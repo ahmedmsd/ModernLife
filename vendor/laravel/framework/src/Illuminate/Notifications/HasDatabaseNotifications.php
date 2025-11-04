eab34ac4a7b0b195f7cede3da9ed9feeffc1e523
Ahmed Elsayed <designer_4ever@hotmail.com>
2025-11-04 16:10:36 +0300

    Update HasDatabaseNotifications.php

--- vendor/laravel/framework/src/Illuminate/Notifications/HasDatabaseNotifications.php
+++ vendor/laravel/framework/src/Illuminate/Notifications/HasDatabaseNotifications.php
@@ -4,31 +4,19 @@ namespace Illuminate\Notifications;
 
 trait HasDatabaseNotifications
 {
-    /**
-     * Get the entity's notifications.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany<DatabaseNotification, $this>
-     */
-    public function notifications()
+
+    public function notifications(): \Illuminate\Database\Eloquent\Relations\MorphMany
     {
         return $this->morphMany(DatabaseNotification::class, 'notifiable')->latest();
     }
 
-    /**
-     * Get the entity's read notifications.
-     *
-     * @return \Illuminate\Database\Query\Builder
-     */
+
     public function readNotifications()
     {
         return $this->notifications()->read();
     }
 
-    /**
-     * Get the entity's unread notifications.
-     *
-     * @return \Illuminate\Database\Query\Builder
-     */
+
     public function unreadNotifications()
     {
         return $this->notifications()->unread();
